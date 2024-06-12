f214cb844bfaa3a68115e4abee849bae76ada7a0
yoshi-zen <yoshi180705@ruri.waseda.jp>
2024-06-13 02:20:14 +0900

    初期設定完了

---
+++ app/src/features/FileTableView/index.tsx
@@ -0,0 +1,16 @@
+export const FileTableView = () => {
+  return (
+    <div className="rounded-lg overflow-hidden border border-solid border-gray-600 divide-y divide-gray-600">
+      <div className="px-2 py-4 bg-gray-900">ユーザー１</div>
+      <div className="p-3">.github</div>
+      <div className="p-3">.husky</div>
+      <div className="p-3">.storybook</div>
+      <div className="p-3">.vscode</div>
+      <div className="p-3">functions</div>
+      <div className="p-3">pages/api</div>
+      <div className="p-3">public</div>
+      <div className="p-3">src</div>
+      <div className="p-3">tailwind.config.js</div>
+    </div>
+  )
+}
